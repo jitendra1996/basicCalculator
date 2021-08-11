39d581602c1bbabf207720397828bda9292c0e9e
jitendra1996 <jitendra199618@gmail.com>
2021-08-11 11:57:06 +0530

    adding outputString method

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,32 +3,37 @@ let currentResult = defaultResult;
 
 const getUserInput = () => parseInt(userInput.value) ;
 
+const outputString = (operator , valueBeforeCal , inputValue)=>{
+    const description = `${valueBeforeCal} ${operator} ${inputValue}`;
+    outputResult(currentResult,description);
+}
+
 function add(){
     const userNumber = getUserInput() ;
-    const description = `${currentResult} + ${userNumber}` ;
+    const initialVal = currentResult ;
     currentResult = currentResult + userNumber;
-    outputResult(currentResult, description);
+    outputString("+" , initialVal , userNumber);
 }
 
 function subtract(){
     const userNumber = getUserInput();
-    const description = `${currentResult} - ${userNumber}`;
-    currentResult = currentResult - userNumber ;
-    outputResult(currentResult,description);
+    const initialVal = currentResult ;
+    currentResult = currentResult - userNumber;
+    outputString("-" , initialVal , userNumber);
 }
 
 function multiply(){
     const userNumber = getUserInput();
-    const description = `${currentResult} * ${userNumber}`;
-    currentResult = currentResult * userNumber ;
-    outputResult(currentResult,description);
+    const initialVal = currentResult ;
+    currentResult = currentResult * userNumber;
+    outputString("*" , initialVal , userNumber);
 }
 
 function divide(){
     const userNumber = getUserInput();
-    const description =`${currentResult} / ${userNumber}`;
-    currentResult = currentResult / userNumber ;
-    outputResult(currentResult,description);
+    const initialVal = currentResult ;
+    currentResult = currentResult / userNumber;
+    outputString("/" , initialVal , userNumber);
 }
 
 addBtn.addEventListener("click",add);
